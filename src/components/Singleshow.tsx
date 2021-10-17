7da50337ab349fefbd926da680899b150b7748bb
minwwgo <37987965+minwwgo@users.noreply.github.com>
2021-10-17 16:38:56 +0100

    fix: single show display

--- src/components/Singleshow.tsx
+++ src/components/Singleshow.tsx
@@ -5,67 +5,68 @@ type SingleShowProps = {
 };
 
 export const Singleshow: React.FC<SingleShowProps> = ({ showDetails }) => {
-  console.log(showDetails);
+  const { name, image, summary, genres, status, premiered } = showDetails;
+  const casts = showDetails?._embedded?.cast;
+  const episodes = showDetails?._embedded?.episodes;
+  const seasons = showDetails?._embedded?.seasons;
+  console.log(episodes);
   return (
     <div className="max-w-7xl mx-auto p-4">
-      {/* {selectedShow && (
+      {showDetails && (
         <div className="grid grid-cols-4 gap-8">
           <img
             className="h-72 w-56 rounded-lg"
-            src={
-              selectedShow?.show?.image?.medium ??
-              selectedShow?.show?.image?.original
-            }
+            src={image?.medium ?? image?.original}
             alt="Movie Poster"
           />
           <div className="col-span-3">
             <div className="flex text-2xl font-bold">
-              <h1>{selectedShow?.show?.name}</h1>
-              <h1>({selectedShow?.show?.premiered?.split("-")[0]})</h1>
+              <h1>{name}</h1>
+              <h1>({premiered?.split("-")[0]})</h1>
             </div>
-            <div>
-              {selectedShow?.show?.summary?.replace(/<\/?[^>]+(>|$)/g, "")}
-            </div>
+            <div>{summary?.replace(/<\/?[^>]+(>|$)/g, "")}</div>
             <div>
               <div className="flex">
                 Cast:
-                {showCast
-                  .map((cast: any) => <div className="pl-4"> {cast.person.name} , </div>)
+                {casts
+                  ?.map((cast: any) => (
+                    <div className="pl-4"> {cast?.person?.name} </div>
+                  ))
                   .slice(0, 2)}
               </div>
 
-              <div>Status: {selectedShow?.show?.status}</div>
-              <div>
-                Genres:{" "}
-                {selectedShow?.show?.genres
-                  ?.map((genre: any) => genre)
-                  .join(",")}
-              </div>
+              <div>Status: {status}</div>
+              <div>Genres: {genres?.map((genre: any) => genre).join(",")}</div>
             </div>
           </div>
         </div>
-      )} */}
+      )}
+      <div>
+        {seasons?.map((cast: any) => (
+          <div>
+            <div className="my-4 font-bold text-2xl"> Season : {cast.number}</div>
 
-      {/* <div>
-        <div className="grid grid-cols-5 p-4 ">
-          {showDetails &&
-            showDetails?.map((episode: any) => {
-              return (
-                <div key={episode.id}>
-                  <div className="flex px-4">
-                    <div className="pr-4"> season: {episode?.season}</div>
-                    <div> episode:{episode?.number}</div>
-                  </div>
-                  <img
-                    className="h-32 w-56"
-                    src={episode?.image?.original}
-                    alt="Movie poster"
-                  />
-                </div>
-              );
-            })}
-        </div>
-      </div> */}
+            <div className="grid grid-cols-5">
+              {episodes &&
+                episodes?.map((episode: any) => {
+                  return (
+                    <div key={episode.id}>
+                      {episode.season === cast.number && (
+                        <div className="m-2">
+                          <img
+                            className="h-32 w-56 "
+                            src={episode?.image?.original}
+                            alt="Movie poster"
+                          />
+                        </div>
+                      )}
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
